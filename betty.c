9a59f4dd4406010bb0e9173dc3337ce5acac72d0
daadesina <daadesina1@gmail.com>
2023-12-05 04:19:17 -0800

    done

---
+++ betty.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * betty - A simple code
+ *
+ * Return: Nothing
+ */
+void betty(void)
+{
+	printf("This is a beautiful code\n");
+}
---
+++ main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - This is a main function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	question1();
+
+	return (0);
+}
